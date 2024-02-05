d94077bf4b8b5103add341cdf2c5c839f18b4008
Reshma_Nalamalapu <rn21aae@herts.ac.uk>
2024-02-05 18:06:35 -0500

    virtual pet

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class VirtualPet
+{
+    public string Type { get; set; }
+    public string Name { get; set; }
+    public int Hunger { get; set; }
+    public int Happiness { get; set; }
+    public int Health { get; set; }
